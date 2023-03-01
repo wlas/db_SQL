8a02cd290f8df78b194e258449154c4a0a3bae1f
SandrLite <mr.wlas@mail.ru>
2023-03-01 19:27:25 +0300

    Task3
    3. Создайте таблицу “orders”, заполните ее значениями. Выберите все заказы. В зависимости от поля order_status выведите столбец full_order_status:
    OPEN – «Order is in open state» ; CLOSED - «Order is closed»; CANCELLED -  «Order is cancelled»

--- Дз2/lesson2.sql
+++ Дз2/lesson2.sql
@@ -32,3 +32,35 @@ SELECT
     IF(count_product < 100, "Маленький заказ",
 		IF(count_product BETWEEN 100 AND 300, "Средний заказ", "Большой заказ")) as "Тип заказа"
 FROM sales;
+
+/*
+3. Создайте таблицу “orders”, заполните ее значениями.
+*/
+CREATE TABLE IF NOT EXISTS orders
+(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    employee_id VARCHAR(5) NOT NULL,
+    amount FLOAT NOT NULL,
+    order_status VARCHAR(10) NOT NULL
+);
+
+INSERT orders(employee_id, amount, order_status)
+VALUES 
+	('e03', 15.00, 'OPEN'),
+    ('e01', 25.50, 'OPEN'),
+    ('e05', 100.70, 'CLOSED'),
+    ('e02', 22.18, 'OPEN'),
+    ('e04', 9.50, 'CANCELLED'); 
+
+/*
+Выберите все заказы. В зависимости от поля order_status выведите столбец full_order_status:
+OPEN – «Order is in open state» ; CLOSED - «Order is closed»; CANCELLED -  «Order is cancelled»
+*/
+
+SELECT *,
+CASE
+	WHEN order_status = 'OPEN' THEN "Order is in open state"
+    WHEN order_status = 'CLOSED' THEN "Order is closed"
+    ELSE "Order is cancelled"
+END AS full_order_status
+FROM orders;
