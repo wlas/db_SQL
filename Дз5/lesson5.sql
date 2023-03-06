8dd4ce02c57af68e6a912ad1db7d3c49c5b64e67
SandrLite <mr.wlas@mail.ru>
2023-03-06 23:00:34 +0300

    Task2
    2. Изменить в существующем представлении порог для стоимости:  пусть цена будет до 30 000 долларов (используя оператор ALTER VIEW).

---
+++ Дз5/lesson5.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE IF NOT EXISTS lesson5;
+USE lesson5;
+
+CREATE TABLE cars
+(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45),
+    cost INT
+);
+INSERT INTO cars(`name`,`cost`)
+VALUES
+	("Audi", 52642), 
+    ("Mercedes", 57127 ),
+    ("Skoda", 9000 ),
+    ("Volvo", 29000),
+	("Bentley", 350000),
+    ("Citroen ", 21000 ), 
+    ("Hummer", 41400), 
+    ("Volkswagen ", 21600);
+
+/*
+	1. Создайте представление, в которое попадут автомобили стоимостью  до 25 000 долларов.
+*/
+CREATE VIEW price_min AS
+SELECT * FROM cars
+WHERE cost < 25000;
+
+SELECT * FROM price_min;
+
+/*
+	2. Изменить в существующем представлении порог для стоимости: 
+    пусть цена будет до 30 000 долларов (используя оператор ALTER VIEW).
+*/
+ALTER VIEW price_min AS 
+SELECT * FROM cars 
+WHERE cost < 30000;
+
+SELECT * FROM price_min;
+
