b136dacafcf6bae9a7bc2ae3afc1964b3d28fbca
SandrLite <mr.wlas@mail.ru>
2023-03-06 23:14:48 +0300

    Task4
    4. Добавьте новый столбец под названием «время до следующей станции».

--- Дз5/lesson5.sql
+++ Дз5/lesson5.sql
@@ -44,3 +44,34 @@ SELECT * FROM cars
 WHERE `name` IN ("Skoda","Audi");
 
 SELECT * FROM only_car;
+
+/*
+   ##########################################
+*/
+
+CREATE TABLE schedule
+(
+	train_id INT NOT NULL,
+    station VARCHAR(20),
+    station_time TIME NOT NULL
+);
+
+INSERT schedule
+VALUES
+	(110, 'San Francisco', '10:00:00'),
+    (110, 'RedWood City', '10:54:00'),
+    (110, 'Palo Alto', '11:02:00'),
+    (110, 'San Jose', '12:35:00'),
+    (120, 'San Francisco', '11:00:00'),
+    (120, 'Palo Alto', '12:49:00'),
+    (120, 'San Jose', '13:30:00');
+    
+SELECT * FROM schedule;    
+/*
+    4. Добавьте новый столбец под названием «время до следующей станции».
+*/
+SELECT
+	schedule.*,
+    TIMEDIFF(LEAD(station_time) OVER (PARTITION BY train_id), station_time) AS time_to_next_station
+FROM schedule;
+
