be4ecded771f4269c1ea20b2a9869174bb3ea1b3
SandrLite <mr.wlas@mail.ru>
2023-03-09 21:15:13 +0300

    Task2
    2.	Выведите только четные числа от 1 до 10 включительно. (Через функцию / процедуру)
    	Пример: 2,4,6,8,10 (можно сделать через шаг +  2: х = 2, х+=2)

--- Дз6/lesson6.sql
+++ Дз6/lesson6.sql
@@ -2,9 +2,9 @@
 	1.	Создайте функцию, которая принимает кол-во сек и форматирует их в кол-во дней, часов, минут и секунд.
 	Пример: 123456 ->'1 days 10 hours 17 minutes 36 seconds '
 */
-delimiter $$
 DROP FUNCTION IF EXISTS convert_time;
 
+delimiter $$
 CREATE FUNCTION convert_time (seconds INT)
 RETURNS CHAR(80)
 DETERMINISTIC 
@@ -25,3 +25,35 @@ END $$
 delimiter ;
 
 SELECT convert_time(12345);
+
+/*
+	2.	Выведите только четные числа от 1 до 10 включительно. (Через функцию / процедуру)
+	Пример: 2,4,6,8,10 (можно сделать через шаг +  2: х = 2, х+=2)
+*/
+DROP PROCEDURE IF EXISTS get_even_numbers;
+
+delimiter $$
+CREATE PROCEDURE get_even_numbers (numb INT)
+BEGIN
+	DECLARE result CHAR(30);
+    DECLARE tmp CHAR(5);    
+    SET result = "";
+    
+    WHILE (numb <= 10) DO
+		BEGIN
+			IF (numb % 2 = 0) THEN
+				IF (numb = 10) THEN
+					SET tmp = numb;
+				ELSE
+					SET tmp = CONCAT(numb, ',');
+				END IF;
+                SET result = CONCAT(result, tmp);
+			END IF;
+            SET numb = numb + 1;
+		END;
+	END WHILE;
+    SELECT result;
+END $$
+delimiter ;
+	
+CALL get_even_numbers(2);
